fe06fba78d0132ba54ac34d670d8a7f94d9e7178
‘fangkuaib’ <fangkuaib@163.com>
2023-01-01 21:44:34 +0800

    static静态文件

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -135,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 告知系统我们的图片位置
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
