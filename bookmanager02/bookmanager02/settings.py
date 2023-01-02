1e27a1e99327aeca1de81490db9666bfd5c43752
‘fangkuaib’ <fangkuaib@163.com>
2023-01-02 20:14:04 +0800

    修改数据库为mysql，重新生成迁移文件并执行迁移

--- bookmanager02/bookmanager02/settings.py
+++ bookmanager02/bookmanager02/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',  # 数据库主机
         'PORT': 3306,  # 数据库端口
         'USER': 'root',  # 数据库用户名
